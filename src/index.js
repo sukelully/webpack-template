3ffa1b03c190161119220a6e1514484b3e8b701f
Luke Sullivan <luke@sukelully.dev>
2025-05-16 12:07:48 +0100

    Set up webpack merge, test npm run deploy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./styles.css";
 import { greeting } from './greeting.js';
 
+const updateGreeting = (function () {
+    const greetingHeader = document.getElementById('greeting-header');
+    greetingHeader.innerHTML = `${greeting}`;
+})();
+
+
 console.log(greeting);
